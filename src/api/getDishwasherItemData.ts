6279fe084dfdc17be873c14ba6b598688cd72be7
Oliver Harris <revilosirrah@gmail.com>
2023-12-13 10:37:43 +0000

    Wire in cal of local files to avoid CORS issue

--- src/api/getDishwasherItemData.ts
+++ src/api/getDishwasherItemData.ts
@@ -1,17 +1,27 @@
 // getDishwasherItemData.tsx
 import axios from "axios";
 
+// will see CORS error on deployment so need mocked data on live
+const currentHostname = window.location.hostname;
+import dishwasherItem from "../api/__mocks__/dishwasherItem.json";
+
 const getDishwasherItemData = async (
   queryString: string | null,
   setDataLoaded: (value: boolean) => void
 ) => {
   try {
-    const response = await axios.get(
-      "https://api.johnlewis.com/mobile-apps/api/v2/products/" + queryString
-    );
-    console.log(response);
-    const data = await response.data;
-    return data;
+    if (currentHostname === "localhost") {
+      const response = await axios.get(
+        "https://api.johnlewis.com/mobile-apps/api/v2/products/" + queryString
+      );
+      console.log(response);
+      const data = await response.data;
+      return data;
+    } else {
+      const response = dishwasherItem;
+      console.log(response);
+      return response;
+    }
   } catch (error) {
     console.log(error);
   } finally {
--- src/api/getDishwashersListData.ts
+++ src/api/getDishwashersListData.ts
@@ -1,16 +1,26 @@
 // getDishwashersListData.tsx
 import axios from "axios";
 
+// will see CORS error on deployment so need mocked data on live
+const currentHostname = window.location.hostname;
+import dishwashersList from "../api/__mocks__/dishwashersList.json";
+
 const getDishwashersListData = async (
   setDataLoaded: (value: boolean) => void
 ) => {
   try {
-    const response = await axios.get(
-      "https://api.johnlewis.com/search/api/rest/v2/catalog/products/search/keyword?q=dishwasher&key=AIzaSyDD_6O5gUgC4tRW5f9kxC0_76XRC8W7_mI"
-    );
-    console.log(response);
-    const data = await response.data;
-    return data;
+    if (currentHostname === "localhost") {
+      const response = await axios.get(
+        "https://api.johnlewis.com/search/api/rest/v2/catalog/products/search/keyword?q=dishwasher&key=AIzaSyDD_6O5gUgC4tRW5f9kxC0_76XRC8W7_mI"
+      );
+      console.log(response);
+      const data = await response.data;
+      return data;
+    } else {
+      const response = dishwashersList;
+      console.log(response);
+      return response;
+    }
   } catch (error) {
     console.log(error);
   } finally {
